0f8ba85d0927adee79734365bc8414999002e6b4
Luis Filipe Dias <luisfiliperdias@gmail.com>
2019-01-02 13:11:31 +0000

    mend

--- src/SumStubImpl.cpp
+++ src/SumStubImpl.cpp
@@ -5,8 +5,8 @@ SumStubImpl::~SumStubImpl() { }
 
 void SumStubImpl::sum(const std::shared_ptr<CommonAPI::ClientId> _client, int32_t _a, int32_t _b, sumReply_t _reply) {
     std::stringstream messageStream;
-    int32_t c = _a + _b;
-    std::cout << _a << "+" <<_b << "=" << c << "'\n";
+    int32_t _c = _a + _b;
+    std::cout << _a << "+" <<_b << "=" << _c << "'\n";
 
-    _reply(c);
+    _reply(_c);
 };
